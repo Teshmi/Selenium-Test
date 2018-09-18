47e7fb0a6bec2eae735f8a3d75bfc7be439c9206
Teshminiketh Syed <teshminiketh.syed@aricent.com>
2018-09-18 17:45:10 +0530

    changes to WebDriver

--- SeleniumTest/src/main/java/com/aricent/SeleniumTest/App.java
+++ SeleniumTest/src/main/java/com/aricent/SeleniumTest/App.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
-    public class App
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class App
     {
         @Test
 
         public static void main(String[] args) {
 
 
-            System.setProperty("webdriver.chrome.driver","/home/edureka/chromedriver");
-            ChromeOptions chromeOptions = new ChromeOptions();
-            WebDriver driver = new ChromeDriver(chromeOptions);
+            WebDriver driver = new FirefoxDriver();
 
             System.out.println("Selenium Web Driver Test");
 
