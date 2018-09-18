dce8aadb5619d2f18f572745ca713cf77898a700
Teshminiketh Syed <teshminiketh.syed@aricent.com>
2018-09-18 12:50:05 +0530

    Commit Selenium Test to master

---
+++ SeleniumTest/src/main/java/com/aricent/SeleniumTest/App.java
@@ -0,0 +1,36 @@
+package com.aricent.SeleniumTest;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+    public class App
+    {
+        @Test
+
+        public static void main(String[] args) {
+
+
+            System.setProperty("webdriver.chrome.driver","/home/edureka/chromedriver");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            WebDriver driver = new ChromeDriver(chromeOptions);
+
+            System.out.println("Selenium Web Driver Test");
+
+
+                    driver.get("http://localhost:3001");
+
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            driver.findElement(By.className("v-button")).click();
+            driver.findElement(By.id("login")).sendKeys("edureka");
+            driver.findElement(By.id("password")).sendKeys("edureka");
+            driver.findElement(By.className("btn btn-primary")).click();
+            //Thread.sleep(5000);
+            System.out.println("Completed");
+            driver.quit();
+        }
+
+    }
+
